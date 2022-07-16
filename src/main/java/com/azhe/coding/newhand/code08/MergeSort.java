19efe9352ec0ea5bf0f73e5183858380b588333f
林哲荣 <78370412@qq.com>
2022-07-16 12:40:51 +0800

    08——>快排、归并排

--- src/main/java/com/azhe/coding/newhand/code08/MergeSort.java
+++ src/main/java/com/azhe/coding/newhand/code08/MergeSort.java
@@ -1,7 +1,7 @@
 package com.azhe.coding.newhand.code08;
 
 /**
- * Description:
+ * Description: 归并排序
  * 递归方法
  * 非递归方法
  *
@@ -9,4 +9,40 @@ package com.azhe.coding.newhand.code08;
  * @date 2022/7/13 10:48 下午
  */
 public class MergeSort {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 归并排序，递归方式
+     *
+     * @param arr
+     */
+    public static void mergeSort1(int[] arr) {
+    }
+
+    /**
+     * 排序
+     * @param arr
+     * @param l
+     * @param r
+     */
+    public static void process(int[] arr, int l, int r) {
+
+    }
+
+    public static void merge(int[] arr, int l, int m, int r) {
+
+    }
+
+    /**
+     * 归并排序2，非递归，步长处理
+     * @param arr
+     */
+    public static void mergeSort2(int[] arr) {
+
+    }
+
+
 }
--- src/main/java/com/azhe/coding/newhand/code08/PartitionAndQuickSort.java
+++ src/main/java/com/azhe/coding/newhand/code08/PartitionAndQuickSort.java
@@ -1,5 +1,7 @@
 package com.azhe.coding.newhand.code08;
 
+import java.util.Arrays;
+
 /**
  * Description:
  * 将小于等于某个值的放左边，其他放右边
@@ -12,9 +14,69 @@ package com.azhe.coding.newhand.code08;
  */
 public class PartitionAndQuickSort {
 
-
-    public static int[] partition(int[] arr, int l, int r) {
-        return null;
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 5, 6, 7, 4, 3, 9, 0, 1, 2, 4, 3};
+//        splitNum1(arr);
+        splitNum2(arr);
+        System.out.println(Arrays.toString(arr));
     }
+
+    /**
+     * 小于等于左边，大于右边
+     */
+    public static void splitNum1(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int p = 0;
+        int i = 0;
+        while (i < arr.length) {
+            if (arr[i] <= arr[arr.length-1]) {
+                swap(arr, i++, p++);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    /**
+     * 小于左边，等于中间，大于右边
+     */
+    public static void splitNum2(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int p = 0; // 小于
+        int q = arr.length-1;  // 大于
+        int i = 0;
+        while (i < q) {
+            if (arr[i] < arr[arr.length-1]) {
+                // 和p位置交换
+                swap(arr, i++, p++);
+            } else if (arr[i] > arr[arr.length-1]) {
+                // 和q位置交换，此时i不变，因为交换后i位置的数据还没比较过
+                swap(arr, i, --q);
+            } else {
+                i++;
+            }
+        }
+        swap(arr, arr.length - 1, q);
+    }
+
+    /**
+     * 交换数组
+     * @param arr
+     * @param l1
+     * @param l2
+     */
+    public static void swap(int[] arr, int l1, int l2) {
+        if (l1 == l2) {
+            return;
+        }
+        int i = arr[l1];
+        arr[l1] = arr[l2];
+        arr[l2] = i;
+    }
+
 
 }
