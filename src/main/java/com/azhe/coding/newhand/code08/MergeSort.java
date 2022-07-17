63ac813bd144b3f5363d1cc74135b10f88b260a8
林哲荣 <78370412@qq.com>
2022-07-18 01:42:57 +0800

    新手班算法结束

--- src/main/java/com/azhe/coding/newhand/code08/MergeSort.java
+++ src/main/java/com/azhe/coding/newhand/code08/MergeSort.java
@@ -11,38 +11,181 @@ package com.azhe.coding.newhand.code08;
 public class MergeSort {
 
     public static void main(String[] args) {
-
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        System.out.println("test begin");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            int[] arr3 = copyArray(arr1);
+            mergeSort1(arr1);
+            mergeSort2(arr2);
+            selectSort(arr3);
+            if (!isEqual(arr1, arr3) || !isEqual(arr2, arr3)) {
+                System.out.println("Oops!");
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println("test end");
+        System.out.println(succeed ? "Nice!" : "Oops!");
     }
 
+    // 插入排序
+    public static void selectSort(int[] arr) {
+        if (arr == null || arr.length <= 1) {
+            return;
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            // i 为需要进行换为位置
+            int minValueIndex = i;
+            for (int j = i+1; j < arr.length ; j++) {
+                // j 为正在比较的位置
+                minValueIndex = arr[minValueIndex] > arr[j]? j : minValueIndex;
+            }
+            swap(arr, i, minValueIndex);
+        }
+    }
+
+
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void swap(int[] arr, int p, int q) {
+        if (p == q) {
+            return;
+        }
+        arr[p] = arr[p] ^ arr[q];
+        arr[q] = arr[p] ^ arr[q];
+        arr[p] = arr[p] ^ arr[q];
+    }
+
+
     /**
-     * 归并排序，递归方式
-     *
+     * 归并排序1，递归
      * @param arr
      */
     public static void mergeSort1(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process(arr, 0, arr.length-1);
     }
 
     /**
-     * 排序
+     * 分别排序，然后合并
      * @param arr
-     * @param l
-     * @param r
+     * @param left
+     * @param right
      */
-    public static void process(int[] arr, int l, int r) {
-
-    }
-
-    public static void merge(int[] arr, int l, int m, int r) {
-
+    public static void process(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        // 中间
+        int middle = left + ((right - left) >> 1);
+        process(arr, left, middle);
+        process(arr, middle + 1, right);
+        merge(arr, left, middle, right);
     }
 
     /**
-     * 归并排序2，非递归，步长处理
+     * 合并排序
+     */
+    public static void merge(int[] arr, int p, int m, int q) {
+        if (p == q) {
+            return;
+        }
+        int[] temp = new int[q-p+1];
+        int p1 = p;
+        int p2 = m+1;
+        int i = 0;
+        // 比较两边
+        while (p1 <= m && p2 <= q) {
+            temp[i++] = arr[p1] <= arr[p2]? arr[p1++] : arr[p2++];
+        }
+        // 比较后，将剩余的数按顺序复制下来
+        while (p1 <= m) {
+            temp[i++] = arr[p1++];
+        }
+        while (p2 <= q) {
+            temp[i++] = arr[p2++];
+        }
+        // 复制
+        for (int j = 0; j < temp.length; j++) {
+            arr[p+j] = temp[j];
+        }
+    }
+
+
+    /**
+     * 归并排序，非递归，步长
      * @param arr
      */
     public static void mergeSort2(int[] arr) {
-
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        // 步长，从1开始
+        int mergeSize = 1;
+        int length = arr.length;
+        while (mergeSize < length) {
+            int p = 0;
+            // p < length-mergeSize+1 条件限制了 p到m时m已经超过length的情况，这种情况不需要排序，本身就有序，
+            // 也避免了越界问题
+            while (p < length && p < length - mergeSize ) {
+                // 中间位置
+                int m = p + mergeSize - 1;
+                // 右边位置
+                int q = m < length - mergeSize - 1? m + mergeSize : length - 1;
+//                int q = m + mergeSize;
+                merge(arr, p, m, q);
+                // 由于上边处理了，q最大只能到 length-1，所以 q+1 不会越界，不用考虑越界情况
+                p = q + 1;
+            }
+            // 考虑越界，可以判断当mergeSize达到长度的一半，则可以跳出循环
+            // 不能等于，因为等于的情况有可能是长度一半后向下取整得到的结果
+            if (mergeSize > (length >>1)) {
+                break;
+            }
+            mergeSize <<=1;
+        }
     }
-
 
 }
--- src/main/java/com/azhe/coding/newhand/code08/PartitionAndQuickSort.java
+++ src/main/java/com/azhe/coding/newhand/code08/PartitionAndQuickSort.java
@@ -1,6 +1,7 @@
 package com.azhe.coding.newhand.code08;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 /**
  * Description:
@@ -15,68 +16,222 @@ import java.util.Arrays;
 public class PartitionAndQuickSort {
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 5, 6, 7, 4, 3, 9, 0, 1, 2, 4, 3};
-//        splitNum1(arr);
-        splitNum2(arr);
-        System.out.println(Arrays.toString(arr));
-    }
-
-    /**
-     * 小于等于左边，大于右边
-     */
-    public static void splitNum1(int[] arr) {
-        if (arr == null || arr.length < 2) {
-            return;
-        }
-        int p = 0;
-        int i = 0;
-        while (i < arr.length) {
-            if (arr[i] <= arr[arr.length-1]) {
-                swap(arr, i++, p++);
-            } else {
-                i++;
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        System.out.println("test begin");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            int[] arr3 = copyArray(arr1);
+            quickSort1(arr1);
+            quickSort2(arr2);
+            selectSort(arr3);
+            if (!isEqual(arr1, arr2) || !isEqual(arr1, arr3)) {
+                System.out.println("Oops!");
+                succeed = false;
+                break;
             }
         }
+        System.out.println("test end");
+        System.out.println(succeed ? "Nice!" : "Oops!");
     }
 
-    /**
-     * 小于左边，等于中间，大于右边
-     */
-    public static void splitNum2(int[] arr) {
-        if (arr == null || arr.length < 2) {
+    // 插入排序
+    public static void selectSort(int[] arr) {
+        if (arr == null || arr.length <= 1) {
             return;
         }
-        int p = 0; // 小于
-        int q = arr.length-1;  // 大于
-        int i = 0;
-        while (i < q) {
-            if (arr[i] < arr[arr.length-1]) {
-                // 和p位置交换
-                swap(arr, i++, p++);
-            } else if (arr[i] > arr[arr.length-1]) {
-                // 和q位置交换，此时i不变，因为交换后i位置的数据还没比较过
-                swap(arr, i, --q);
-            } else {
-                i++;
+        for (int i = 0; i < arr.length - 1; i++) {
+            // i 为需要进行换为位置
+            int minValueIndex = i;
+            for (int j = i+1; j < arr.length ; j++) {
+                // j 为正在比较的位置
+                minValueIndex = arr[minValueIndex] > arr[j]? j : minValueIndex;
             }
+            swap(arr, i, minValueIndex);
         }
-        swap(arr, arr.length - 1, q);
     }
 
     /**
-     * 交换数组
+     * 小于等于 | 大于
      * @param arr
-     * @param l1
-     * @param l2
      */
-    public static void swap(int[] arr, int l1, int l2) {
-        if (l1 == l2) {
+    public static void partition1(int[] arr) {
+        if (arr == null || arr.length < 2) {
             return;
         }
-        int i = arr[l1];
-        arr[l1] = arr[l2];
-        arr[l2] = i;
+        int lessEquip = 0;
+        int i = 0;
+        int lastIndex = arr.length - 1;
+        while (i < lastIndex) {
+            // 符合条件
+            if (arr[i] <= arr[lastIndex]) {
+                // 交换
+                swap(arr, i, lessEquip++);
+            }
+            i++;
+        }
+        swap(arr, lessEquip, lastIndex);
     }
+
+    /**
+     * 小于 | 等于 | 大于
+     * @param arr
+     */
+    public static void partition2(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int i = 0;
+        int less = 0;
+        int more = arr.length - 2;
+        int lastIndex = arr.length - 1;
+        while (i <= more) {
+            if (arr[i] < arr[lastIndex]) {
+                swap(arr, i++, less++);
+            } else if (arr[i] > arr[lastIndex]) {
+                swap(arr, i, more--);
+            } else {
+                i++;
+            }
+        }
+        swap(arr, ++more, lastIndex);
+        System.out.println(less);
+        System.out.println(more);
+    }
+
+    public static void swap(int[] arr, int p, int q) {
+        if (p == q) {
+            return;
+        }
+        arr[p] = arr[p] ^ arr[q];
+        arr[q] = arr[p] ^ arr[q];
+        arr[p] = arr[p] ^ arr[q];
+    }
+
+    /**
+     * 快排
+     * @param arr
+     */
+    public static void quickSort1(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process(arr, 0, arr.length-1);
+
+    }
+
+    public static void process(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int[] equal = partition(arr, left, right);
+        if (equal[0] > left) {
+            process(arr, left, equal[0]-1);
+        }
+        if (equal[1] < right) {
+            process(arr, equal[1] + 1, right);
+        }
+    }
+
+    /**
+     * 分层
+     */
+    public static int[] partition(int[] arr, int p, int q) {
+        int less = p;
+        int more = q-1;
+        int i = p;
+        // 最后一个值
+        int t = arr[q];
+        while (i <= more) {
+            if (arr[i] < t) {
+                swap(arr, i++, less++);
+            } else if (arr[i] > t) {
+                // 交换后，i不用自增，因为交换后i位置的数值是还没比较过的
+                swap(arr, i, more--);
+            } else {
+                i++;
+            }
+        }
+        swap(arr, ++more, q);
+        return new int[]{less, more};
+    }
+
+    /**
+     * 任务类
+     */
+    public static class Job {
+        int left;
+        int right;
+        public Job(int left, int right) {
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * 快排，任务方式
+     * @param arr
+     */
+    public static void quickSort2(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        Stack<Job> stack = new Stack<>();
+        // 推任务
+        stack.push(new Job(0, arr.length-1));
+        while (!stack.isEmpty()) {
+            Job job = stack.pop();
+            int[] partition = partition(arr, job.left, job.right);
+            if (partition[0] > job.left) {
+                stack.push(new Job(job.left, partition[0] - 1));
+            }
+            if (partition[1] < job.right) {
+                stack.push(new Job(partition[1] + 1, job.right));
+            }
+        }
+
+    }
+
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
 
 }
